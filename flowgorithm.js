6fe0355491f2e3e95a23acba218aa124adc83d6e
Andrea Vallorani <andrea.vallorani@gmail.com>
2024-02-04 15:22:17 +0100

    Update flowgorithm.js
    update license

--- flowgorithm.js
+++ flowgorithm.js
@@ -1,4 +1,20 @@
 /**
+ * This file is part of FlowgorithmJS.
+ * Copyright (C) 2018-2024 Andrea Vallorani
+
+ * FlowgorithmJS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * FlowgorithmJS
  * @version 0.17a1
  * @date 2020-05-02
